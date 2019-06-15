91af19379beb61a14d164a6f1cc8116d146e5c66
madjid <madjidyous@gmail.com>
2019-06-15 12:44:29 +0200

    firt version react native  + login + sign up  whith cognito

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+	apiGateway: {
+		REGION: 'eu-west-1',
+		URL: 'https://a6vq2lm90j.execute-api.eu-west-1.amazonaws.com/dev/users/putTable'
+	},
+	cognito: {
+		REGION: 'eu-west-1',
+		USER_POOL_ID: 'eu-west-1_hOyN8yuau',
+		APP_CLIENT_ID: '3tghkk6r3e37qnbapd7lsm626g',
+		IDENTITY_POOL_ID: 'eu-west-1:0ccb6d61-47f1-40b9-a131-d922f1236dff',
+	}
+	};
